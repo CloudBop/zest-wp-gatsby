3edd954da8d137b9a5706dd6f68c4ea0e36ba1e8
Cloudbop <boppincolin@gmail.com>
2020-01-05 22:54:46 +0000

    added breadcrumb + page-hero / featured img

--- src/components/BreadCrumb.js
+++ src/components/BreadCrumb.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import { PropTypes } from 'prop-types';
 
-const BreadCrumb = () => <div>BreadCrumb</div>;
+import { BreadCrumbWrapper } from './styles/BreadCrumbStyles';
+
+const BreadCrumb = ({ parent }) => (
+  <div className="container">
+    <div className="row">
+      <div className="col-lg-9 offset-lg-3">
+        <BreadCrumbWrapper>
+          <Link to="/">
+            <span>MakeDyanmic</span>
+          </Link>
+          <span className="divider">/</span>
+          {parent ? (
+            <React.Fragment>
+              <Link to={parent.link}>
+                <span dangerouslySetInnerHTML={{ __html: parent.title }} />
+              </Link>
+              <span className="divider">/</span>
+            </React.Fragment>
+          ) : null}
+        </BreadCrumbWrapper>
+      </div>
+    </div>
+  </div>
+);
+
+BreadCrumb.propTypes = {
+  parent: PropTypes.object
+};
 
 export default BreadCrumb;
