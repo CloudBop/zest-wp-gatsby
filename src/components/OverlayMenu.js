7320ffe958fb60ac3e25a90792ed1292f1ad6357
Cloudbop <boppincolin@gmail.com>
2020-01-05 16:23:33 +0000

    added navigation component and added proptypes to menuOverlay

--- src/components/OverlayMenu.js
+++ src/components/OverlayMenu.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
-
+//
 import WhiteLogo from '../images/tango_logo_white.svg';
 import CloseButton from '../images/tango_close_button.svg';
+//
+import PropTypes from 'prop-types';
 
 import { Overlay } from './styles/OverlayMenuStyles';
 
 const OverlayMenu = ({ menuOpen, callback }) => {
-  const {
-    menu: {
-      edges: [{ node: menu }],
-    },
-  } = useStaticQuery(
+  /**
+   * static query, can be called from any component but with no variables
+   * filter by menu_id
+   */
+  const { menu: { edges: [ { node: menu } ] } } = useStaticQuery(
     graphql`
       query OverlayMenu {
-        menu: allWordpressWpApiMenusMenusItems(
-          filter: { wordpress_id: { eq: 5 } }
-        ) {
+        menu: allWordpressWpApiMenusMenusItems(filter: { wordpress_id: { eq: 5 } }) {
           totalCount
           edges {
             node {
@@ -44,18 +44,17 @@ const OverlayMenu = ({ menuOpen, callback }) => {
             </li>
           ))}
         </ul>
-        <div
-          className="closeButton"
-          onClick={callback}
-          role="button"
-          tabIndex="0"
-          onKeyDown={callback}
-        >
+        <div className="closeButton" onClick={callback} role="button" tabIndex="0" onKeyDown={callback}>
           <img src={CloseButton} alt="tango-close-button" />
         </div>
       </div>
     </Overlay>
   );
+};
+
+OverlayMenu.propTypes = {
+  menuOpen: PropTypes.bool.isRequired,
+  callback: PropTypes.func.isRequired
 };
 
 export default OverlayMenu;
