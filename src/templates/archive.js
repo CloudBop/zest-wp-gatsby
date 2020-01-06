88e5306187677202ce63c3999813f7aa225ae5fa
Cloudbop <boppincolin@gmail.com>
2020-01-06 00:01:50 +0000

    added pagination component

--- src/templates/archive.js
+++ src/templates/archive.js
@@ -8,30 +8,18 @@ import BreadCrumb from '../components/BreadCrumb';
 import Pagination from '../components/archive/Pagination';
 import PageHero from '../components/PageHero';
 
-import {
-  PageContent,
-  StyledH2,
-  StyledDate,
-  StyledReadMore,
-} from './styles/archiveStyles';
+import { PageContent, StyledH2, StyledDate, StyledReadMore } from './styles/archiveStyles';
 
 const archiveTemplate = ({
   data: { file, allWordpressPost },
-  pageContext: {
-    catId,
-    catName,
-    catSlug,
-    categories,
-    humanPageNumber,
-    numberOfPages,
-  },
+  pageContext: { catId, catName, catSlug, categories, humanPageNumber, numberOfPages }
 }) => (
   <Layout>
     <PageHero img={file.childImageSharp.fluid} />
     <BreadCrumb
       parent={{
         link: '/trends/alla-trendspaningar',
-        title: 'trends',
+        title: 'trends'
       }}
     />
 
@@ -39,38 +27,31 @@ const archiveTemplate = ({
       <div className="row" style={{ marginBottom: '40px' }}>
         <ArchiveSidebar catId={catId} categories={categories} />
         <PageContent className="col-lg-9">
-          <Pagination
-            catSlug={catSlug}
-            page={humanPageNumber}
-            totalPages={numberOfPages}
-          />
+          <Pagination catSlug={catSlug} page={humanPageNumber} totalPages={numberOfPages} />
           <h1 dangerouslySetInnerHTML={{ __html: catName }} />
           {allWordpressPost.edges.map(post => (
+            //
             <article key={post.node.id} className="entry-content">
               <Link to={`/trends/${post.node.slug}/`}>
-                <StyledH2
-                  dangerouslySetInnerHTML={{ __html: post.node.title }}
-                />
+                <StyledH2 dangerouslySetInnerHTML={{ __html: post.node.title }} />
               </Link>
               <StyledDate
                 dangerouslySetInnerHTML={{
-                  __html: post.node.date.replace(/\W+/g, ' '),
+                  // remove comma, replace with space
+                  __html: post.node.date.replace(/\W+/g, ' ')
                 }}
               />
+              {/** EXCERPT */}
               <p dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
-              <StyledReadMore to={`/trends/${post.node.slug}`}>
-                Read More
-              </StyledReadMore>
+
+              <StyledReadMore to={`/trends/${post.node.slug}`}>Read More</StyledReadMore>
+
               <div className="dot_divider">
                 <hr />
               </div>
             </article>
           ))}
-          <Pagination
-            catSlug={catSlug}
-            page={humanPageNumber}
-            totalPages={numberOfPages}
-          />
+          <Pagination catSlug={catSlug} page={humanPageNumber} totalPages={numberOfPages} />
         </PageContent>
       </div>
     </div>
@@ -78,14 +59,10 @@ const archiveTemplate = ({
 );
 
 export default archiveTemplate;
-
+// filter posts for archive categories & get image
 export const pageQuery = graphql`
   query($catId: String!, $skip: Int!, $limit: Int!) {
-    allWordpressPost(
-      filter: { categories: { elemMatch: { id: { eq: $catId } } } }
-      skip: $skip
-      limit: $limit
-    ) {
+    allWordpressPost(filter: { categories: { elemMatch: { id: { eq: $catId } } } }, skip: $skip, limit: $limit) {
       edges {
         node {
           id
